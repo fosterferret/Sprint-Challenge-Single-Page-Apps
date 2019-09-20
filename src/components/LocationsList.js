b28f3d27d5f545a662612cfdcaa1ac0d02c7cb1a
Francis Bulus <notchera@gmail.com>
2019-09-20 09:36:12 +0100

    added useeffect to locations list component to keep track of and act on changes in locations

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -4,4 +4,15 @@ import axios from 'axios';
 
 export default function LocationsList() {
     const [locations, setLocations] = useState([]);
+    
+    useEffect(() => {
+        axios
+          //.get(`https://rickandmortyapi.com/api/location/`)
+          .then(response => {
+            setLocations(response.data.results);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, [locations]);
 }
