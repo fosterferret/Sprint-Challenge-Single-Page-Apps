7fcc747e16b6eee5c360db22f6e948cf01ec778a
Francis Bulus <notchera@gmail.com>
2019-09-20 08:41:28 +0100

    built out header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,22 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+    <header className="header">
+      <div className="header-left">
+        <img
+          src={require("./images/logo.png")}
+          alt="rick and morty header"
+          className="header-logo"
+        />
+        <h2>I GOT ONE RIGHT HERE - GRAB MY TERRY FLAP!</h2>
+      </div>
+      <div className="header-right">
+        <img
+          src={require("./images/rm-eye.png")}
+          className="header-img"
+          alt="rick-and-morty"
+        />
+      </div>
     </header>
   );
 }
