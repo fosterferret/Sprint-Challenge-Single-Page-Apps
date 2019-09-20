9fbc8d547a3b161055e5f43cfb0f0b020d69ce55
Francis Bulus <notchera@gmail.com>
2019-09-20 08:22:39 +0100

    built out character card component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  return (
+    <>
+      {/* <img className="rm-logo" alt="logo"/> */}
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <p>
+        {species} - {status}
+      </p>
+      {/* <img className="location-icon" alt="location-icon"/> */}
+      <p>{location}</p>
+      <div className="footer">
+        <p>{episodes.length} Episode(s)</p>
+      </div>
+    </>
+  );
 }
