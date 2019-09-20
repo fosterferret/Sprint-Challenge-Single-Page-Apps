57f077288b141f355b0da8481b52ba205606b849
Francis Bulus <notchera@gmail.com>
2019-09-20 08:52:42 +0100

    made style additions and changes to header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route path="/characters" component={CharacterList} />
     </main>
   );
 }
