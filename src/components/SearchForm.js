4e922d9d0f22a33b02826ff2b1a2e6596135371e
Francis Bulus <notchera@gmail.com>
2019-09-20 09:05:01 +0100

    added states to searchform component to track user input

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import CharacterCard from "./CharacterCard";
 
 export default function SearchForm() {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("");
  
   return (
     <section className="search-form">
